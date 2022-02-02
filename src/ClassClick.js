783cd3969c79ebf7b634c00cd83c191e9bb4fc81
abhinabgp <87983611+abhinabgp@users.noreply.github.com>
2022-02-02 15:51:00 +0530

    first commit

---
+++ src/ClassClick.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export class ClassClick extends Component {
+    clickHandler()
+    {
+        console.log("clicked the button ")
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickHandler}>click me </button>
+            </div>
+        )
+    }
+}
+
+export default ClassClick
